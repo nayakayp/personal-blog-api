2f5810da7959a6e0d434efdb13384162d24261a8
Nayaka Yoga Pradipta <nayakayp@gmail.com>
2024-03-18 04:46:06 +0700

    feat: connect to MongoDB

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.get("/posts", (req, res) => {
-  res.send({ msg: "success" });
-});
+mongoose.connect(process.env.MONGODB_URI);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.on("connected", () => console.log("MongoDB connected"));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
