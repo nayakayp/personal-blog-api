df754cef3837780a740d50ed71929b54cae02fcf
Nayaka Yoga Pradipta <nayakayp@gmail.com>
2024-03-18 04:43:06 +0700

    feat: first route

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.get("/posts", (req, res) => {
+  res.send({ msg: "success" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
